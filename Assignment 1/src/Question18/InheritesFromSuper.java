a8633abebf6a10140bddc69f93f89d2d142f6281
Mazhar <mazkataz@hotmail.com>
2017-06-19 16:25:22 -0400

    problem18

--- Assignment 1/src/Question18/InheritesFromSuper.java
+++ Assignment 1/src/Question18/InheritesFromSuper.java
@@ -3,7 +3,66 @@ package Question18;
 public class InheritesFromSuper {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Child a = new Child();
+		String str = "hey";
+		
+		
+		if (a.upperCase(str) ) System.out.println("true");
+		
+		System.out.println(a.toUpper(str) );		
+		
+		
+	}
+
+	public static class Child extends Parent {
+
+		Child(){;}
+		public boolean upperCase(String str) {
+
+			for (int x = 0; x < str.length(); x++)
+				if (str.charAt(x) >= 'A' && str.charAt(x) <= 'Z')
+					return true;
+
+			return false;
+
+		}
+		
+		
+		public String toUpper(String str){
+			
+			String uppercase = str.toUpperCase();
+			
+			return uppercase;
+			
+			
+		}
+
+		public void outputString(String str){
+			
+			int sum=0; 
+			
+				sum=Integer.parseInt(str);
+				
+			
+			
+		
+			System.out.println(sum+10);
+			
+		}
+		
+
+	}
+
+	public static abstract class Parent {
+
+		Parent(){;}
+		
+		public abstract boolean upperCase(String str);
+
+		public abstract String toUpper(String str);
+
+		public abstract void outputString(String str);
 
 	}
 

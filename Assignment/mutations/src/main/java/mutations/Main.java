9435751798ccf4fde364d84fd000b21e9a09686d
alan <alancao101@gmail.com>
2017-06-19 11:53:20 -0400

    Updated mutation

--- Assignment/mutations/src/main/java/mutations/Main.java
+++ Assignment/mutations/src/main/java/mutations/Main.java
@@ -2,7 +2,7 @@ package mutations;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		System.out.println(MinMutation.mutation("AACCGGTT", "AAACGGTA"));
 
 	}
--- Assignment/mutations/src/main/java/mutations/MinMutation.java
+++ Assignment/mutations/src/main/java/mutations/MinMutation.java
@@ -8,35 +8,32 @@ public class MinMutation {
 	
 	public static final String GENESTRING = "ACGT";
 
-	public static int mutation(String start, String end){
-		
+	public static int mutation(String start, String end) {
+
 		if (start == null || end == null || start.length() != 8 || end.length() != 8)
 			return -1;
-		
+
 		if (start.equals(end))
 			return 0;
-		
-		int len = 0;
+
+
 		ArrayList<String> list = new ArrayList<String>();
-		
+
 		String temp = start;
-		
-		for (int i = 0; i < start.length(); i++){
-			if (start.compareTo(end) == 0){
-				list.add(temp);
-				break;
-			}
-			
+		int len = 0;
+
+		for (int i = 0; i < start.length(); i++) {
+
 			if (start.charAt(i) != end.charAt(i)){
-				temp = temp.substring(0, i - 1) + end.charAt(i) + temp.substring(i+1);
+				temp = temp.substring(0, i) + end.charAt(i) + temp.substring(i + 1);
+				len++;
 				list.add(temp);
 			}
 		}
-		
-		
-		for(String i : list){
-			System.out.println(i);
-		}
+
+		System.out.println("Start: " + start);
+		System.out.println("End: " + end);
+		System.out.println("bank: " + list.toString());
 		
 		return len;
 	}

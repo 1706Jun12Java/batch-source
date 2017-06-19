903113aa5704d7102329788101688bfc98c76a43
sobatao <sobatao@gmail.com>
2017-06-19 11:26:16 -0400

    Add files via upload

---
+++ CodeChallenge/src/main/java/code/challenge/week1/GeneMutation.java
@@ -0,0 +1,24 @@
+package code.challenge.week1;
+
+public class GeneMutation {
+	
+	String origin = "AACCGGTT";
+	String end = "AAACGGTA";
+	String Bank = "AACCGGTT , AAACGGTA";
+	
+	GeneMutation(){
+		
+	}
+	
+	//Putting the gene into the mutator will compare the length of both start and end and bank
+	//Using string start and end compare the difference in each pass then store in a hash key,pair
+	
+	public static void mutator(String startseq, String endseq, String[] bank){
+	
+			
+		
+	}
+	
+	//needed to be put in folder. 
+
+}

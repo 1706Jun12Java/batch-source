f1f742a779e46d66bceb57fb238c4fe30c501c57
iRzLi <irzli1995@gmail.com>
2017-07-07 10:40:14 -0400

    "removed"

--- JDBCBank/src/com/revature/util/ConnectionUtil.java
+++ JDBCBank/src/com/revature/util/ConnectionUtil.java
@@ -9,12 +9,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-	public static Connection getConnection() throws SQLException{
-		String url="jdbc:oracle:thin:@javademo.c1eldqv7hqgd.us-east-2.rds.amazonaws.com:1521:ORCL";
-		String username="Admin";
-		String password="password1q";
-		return DriverManager.getConnection(url,username,password);
-	}
+	
 	public static Connection getConnectionFromFile(String filename)
 			throws IOException, SQLException{
 		Properties prop = new Properties();

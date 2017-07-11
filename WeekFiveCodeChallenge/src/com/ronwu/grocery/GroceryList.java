78b3ef023363cda037595d2d0a553dcd4b94efee
Ron Wu <rcw278@nyu.edu>
2017-07-11 16:53:58 -0400

    load old list

--- WeekFiveCodeChallenge/src/com/ronwu/grocery/GroceryList.java
+++ WeekFiveCodeChallenge/src/com/ronwu/grocery/GroceryList.java
@@ -27,6 +27,7 @@ public class GroceryList extends HttpServlet {
  
     @Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	
     	resp.sendRedirect("index.html");
 	}
  
--- WeekFiveCodeChallenge/src/com/ronwu/grocery/ReadWriteList.java
+++ WeekFiveCodeChallenge/src/com/ronwu/grocery/ReadWriteList.java
@@ -48,4 +48,5 @@ public class ReadWriteList {
 			e.printStackTrace();
 		} 
 	}
+	 
 }

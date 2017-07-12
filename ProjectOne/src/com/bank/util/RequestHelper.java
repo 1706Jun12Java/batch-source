4c0ecc1354f54f9d67797d9b66e36b7dc7feed2f
lizhou <alancao101@gmail.com>
2017-07-11 21:10:19 -0400

    stuff

---
+++ ProjectOne/src/com/bank/util/RequestHelper.java
@@ -0,0 +1,17 @@
+package com.bank.util;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RequestHelper {
+	
+	public static String process(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+		switch(req.getParameter("route")){
+		default:
+			return "404.html";
+		}
+	}
+
+}

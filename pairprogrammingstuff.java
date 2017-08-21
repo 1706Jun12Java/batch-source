821a5a41cf8e126335d3aa068507d9bc47127e1c
adavis20 <hotblade33@hotmail.com>
2017-08-21 16:52:32 -0400

    More robust solution

--- pairprogrammingstuff.java
+++ pairprogrammingstuff.java
@@ -4,13 +4,20 @@ public class Primechecker {
 Primechecker(){
 super();
 }
-
-boolean checkPrime(int n) {
+public static void main(String[] args){
+	checkPrime(5);
+	checkPrime(6);
+	checkPrime(2);
+	checkPrime(13);
+}
+void checkPrime(int n) {
     for(int i=2;i<n/2;i++) {
-        if(n%i==0)
-         return false;
+        if(n%i==0){
+        System.out.println(false);
+	return;
+	}
     }
-    return true;
+    System.out.println(true);
 }
 
 }
